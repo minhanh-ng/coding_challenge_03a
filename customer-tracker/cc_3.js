20856fd0a35e6bd429bc97f427a48ba88d2ab0d9
Minh Anh Nguyen <minhanhn@usf.edu>
2025-09-13 13:48:07 -0400

    push, shift, and update Customers array

--- customer-tracker/cc_3.js
+++ customer-tracker/cc_3.js
@@ -1,1 +1,17 @@
-let customers= ["name", "email",["purchase1", "purchase2", "purchase3"]];
+const customers= [
+    {name: "Nick", email: "nick@email.com", purchases: ["Paper", "Pen", "Pencil"]},
+    {name: "Chimmie", email: "chimmie@email.com", purchases: ["Candle", "Pillow", "Chocolate"]},
+    {name: "Dankey", email: "dankey@email.com", purchases: ["Ramen", "Chips", "Matcha"]}
+];
+
+customers.push(
+    {name: "Cinnamoroll", email: "cinnamoroll@email.com", purchases:["Honey", "Nutella", "Strawberry"]}
+);
+
+customers.shift();
+
+customers[1].email = "chimmie@gmail.com";
+
+customers[2].purchases.push("Hot Chocolate");
+
+console.log(customers)
